1e024a189767172e5709d5a52f7225573f2e7085
freejiebao <jiexiao@pku.edu.cn>
2019-11-13 14:14:46 +0100

    update fiducial definition

--- Configurations/ssww/2016_wz/variables.py
+++ Configurations/ssww/2016_wz/variables.py
@@ -32,23 +32,45 @@ variables['mll_v3']  = {   'name': 'mll',            #   variable name
                         'xaxis' : 'mll [GeV]',  #   x axis name
                         'fold' : 3
                         }
-variables['mll_v2']  = {   'name': 'mll',            #   variable name
-                           'range' : (80, 0. ,800),    #   variable range
-                           'xaxis' : 'mll [GeV]',  #   x axis name
-                           'fold' : 3
-                           }
-
 
 variables['mjj']  = {  'name': 'mjj',
                        'range': ([500,800,1100,1500,2000],),  #for 500 < mjj < 1000
                        'xaxis': 'mjj [GeV]',
                        'fold': 3
                        }
-variables['mjj_v2']  = {  'name': 'mjj',
-                          'range': (10,150,500),  #for 500 < mjj < 1000
-                          'xaxis': 'mjj [GeV]',
-                          'fold': 3
-                          }
+variables['pt1']  = {   'name': 'Alt$(Lepton_pt[0],-9999.)',
+                        'range' : (10,30.,300),
+                        'xaxis' : 'p_{T} 1st lep',
+                        'fold'  : 3
+                        }
+variables['pt1_v2']  = {   'name': 'Alt$(Lepton_pt[0],-9999.)',
+                           'range' : (12,30.,300),
+                           'xaxis' : 'p_{T} 1st lep',
+                           'fold'  : 3
+                           }
+
+variables['pt2']  = {   'name': 'Alt$(Lepton_pt[1],-9999.)',
+                        'range' : (10,30.,180),
+                        'xaxis' : 'p_{T} 2nd lep',
+                        'fold'  : 3
+                        }
+variables['pt2_v2']  = {   'name': 'Alt$(Lepton_pt[1],-9999.)',
+                           'range' : (10,30.,150),
+                           'xaxis' : 'p_{T} 2nd lep',
+                           'fold'  : 3
+                           }
+
+variables['jetpt1']  = {   'name': 'Alt$(Jet_pt[0],-9999.)',
+                           'range' : (10,30.,350),
+                           'xaxis' : 'p_{T} 1st jet',
+                           'fold'  : 3
+                           }
+
+variables['jetpt2']  = {   'name': 'Alt$(Jet_pt[1],-9999.)',
+                           'range' : (10,30.,350),
+                           'xaxis' : 'p_{T} 2nd jet',
+                           'fold'  : 3
+                           }
 
 variables['mjj_v3']  = {  'name': 'mjj',
                           'range': (15, 500. ,2000),  #for 500 < mjj < 1000
