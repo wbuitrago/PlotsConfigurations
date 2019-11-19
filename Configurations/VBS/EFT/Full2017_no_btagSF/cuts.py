6fe05ebae6740fce0feb9cde7eec7e3105df3d9c
dbrambilla13 <daniele.bram@gmail.com>
2019-11-19 16:15:35 +0100

    removing btagSF for test

---
+++ Configurations/VBS/EFT/Full2017_no_btagSF/cuts.py
@@ -0,0 +1,32 @@
+# cuts
+
+## Top control regions
+# 2 or more jets
+# OS e+- & mu-+ (different flavour)
+cuts['top']  = { 
+      'expr' : '  nJet >= 2 \
+                  && Lepton_pdgId[0]*Lepton_pdgId[1] == -11*13 \
+                  && Lepton_pt[0]>20 \
+                  && Lepton_pt[1]>10 \
+                  && (abs(Lepton_pdgId[0])==13 || Lepton_pt[0]>25) \
+                  && (abs(Lepton_pdgId[1])==13 || Lepton_pt[1]>13) \
+                  && (nLepton>=2 && Alt$(Lepton_pt[2],0)<10) \
+                  && abs(Lepton_eta[0])<2.5 && abs(Lepton_eta[1])<2.5 \
+                  '
+}
+
+
+
+# kept as example of categories but not needed anymore.. future removal?
+
+# ## Top control regions
+# cuts['hww2l2v_13TeV_top']  = { 
+#    'expr' : 'topcr',
+#     # Define the sub-categorization of topcr
+#    'categories' : {
+#       '0j' : 'zeroJet',
+#       '1j' : 'oneJet && Alt$(CleanJet_pt[1],0)<30',
+#       '2j' : 'mjj<400 && multiJet',
+#    }
+# }
+
