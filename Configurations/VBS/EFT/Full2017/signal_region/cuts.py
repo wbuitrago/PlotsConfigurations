47c4fc370a1850d58715f9a2136cfb6e19b23789
dbrambilla13 <daniele.bram@gmail.com>
2020-02-10 16:04:50 +0100

    correcting supercut

--- Configurations/VBS/EFT/Full2017/signal_region/cuts.py
+++ Configurations/VBS/EFT/Full2017/signal_region/cuts.py
@@ -8,17 +8,20 @@ and_separator = ' && '
 
 # 2 jets and 2 leptons with p_t > 30 GeV and eta comprised in detector acceptance
 
-supercut_vector = [     'nLepton > 1',
-                        'Lepton_pt[0] > 18',
-                        'Lepton_pt[1] > 8',    
+supercut_vector = [     'nLepton >= 2',
+                        '3rd_lep_veto',
+                        'mll > 12',
+                        'Lepton_pt[0] > 20',
+                        'Lepton_pt[1] > 10',   
+                        '(abs(Lepton_pdgId[0])==13 || Lepton_pt[0]>25)', 
+                        '(abs(Lepton_pdgId[1])==13 || Lepton_pt[1]>13)',
                         '((fabs(Lepton_eta[0]) < 2.5 && abs(Lepton_pdgId[0])==11) || (fabs(Lepton_eta[0]) < 2.4 && abs(Lepton_pdgId[0])==13))',
-                        '((fabs(Lepton_eta[1]) < 2.5 && abs(Lepton_pdgId[1])==11) || (fabs(Lepton_eta[1]) < 2.4 && abs(Lepton_pdgId[1])==13))',                        'Alt$(CleanJet_pt[0],-9999.) > 30', 
-                        'nCleanJet > 1',
+                        '((fabs(Lepton_eta[1]) < 2.5 && abs(Lepton_pdgId[1])==11) || (fabs(Lepton_eta[1]) < 2.4 && abs(Lepton_pdgId[1])==13))',                        
+                        'nCleanJet >= 2',
                         'CleanJet_pt[0] > 30', 
                         'CleanJet_pt[1] > 30', 
                         'fabs(CleanJet_eta[0]) < 5',
                         'fabs(CleanJet_eta[1]) < 5',
-                        'mll > 10',
                   ]
 
 # supercut definition
