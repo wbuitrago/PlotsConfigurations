4a8993da5b62d7c32c0d3129656c1e8eb614ddb8
dbrambilla13 <daniele.bram@gmail.com>
2020-02-10 18:24:09 +0100

    first commit for 2018 configuration

---
+++ Configurations/VBS/EFT/2018_template/configuration.py
@@ -0,0 +1,48 @@
+# example of configuration file
+
+treeName= 'Events'
+
+date = '_feb10'
+
+version = '_v0'
+
+tag = 'VBS_SS'  + date + version
+
+
+# used by mkShape to define output directory for root files
+outputDir = 'rootFile' + '_' + tag
+
+# file with TTree aliases
+aliasesFile = 'aliases.py'
+
+# file with list of variables
+variablesFile = 'variables.py'
+
+# file with list of cuts
+cutsFile = 'cuts.py'
+#cutsFile = 'cuts_forPlots.py'
+
+# file with list of samples
+samplesFile = 'samples.py'
+
+# file with list of samples
+plotFile = 'plot.py'
+
+# luminosity to normalize to (in 1/fb)
+lumi = 59.74 
+
+# used by mkPlot to define output directory for plots
+# different from "outputDir" to do things more tidy
+outputDirPlots = 'plotVBS' + tag
+
+
+# used by mkDatacards to define output directory for datacards
+outputDirDatacard = 'datacards' + tag
+
+
+# structure file for datacard
+structureFile = 'structure.py'
+
+
+# nuisances file for mkDatacards and for mkShape
+nuisancesFile = 'nuisances.py'
