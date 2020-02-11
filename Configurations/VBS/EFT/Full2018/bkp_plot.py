327c5279676b04f149abf8af2f3c2387959072f4
dbrambilla13 <daniele.bram@gmail.com>
2020-02-11 16:48:23 +0100

    renaming folder in Full2018

---
+++ Configurations/VBS/EFT/Full2018/bkp_plot.py
@@ -0,0 +1,218 @@
+# plot configuration
+
+# groupPlot = {}
+#
+# Groups of samples to improve the plots.
+# If not defined, normal plots is used
+#
+
+import copy
+
+origcuts=copy.deepcopy(cuts)
+
+print origcuts
+cuts = []
+
+
+for cut in origcuts:
+    print cut
+    for cat in origcuts[cut]['categories']:
+        cuts.append(cut+"_"+cat)
+
+print cuts
+
+# groupPlot = {}
+#
+# Groups of samples to improve the plots (merge different sample during plot).
+# If not defined, normal plots is used
+#
+Red=632; Violet=880; Green=416; Orange=800; Yellow=400; Azure=860
+groupPlot['WZ_QCD']  = {
+    'nameHR' : "WZ QCD",
+    'isSignal' : 0,
+    'color'    : ROOT.kRed, # kViolet+10
+    'samples'  : ['WLLJJ_QCD','WZTo2L2Q']
+}
+groupPlot['WZ_EWK']  = {
+    'nameHR' : "WZ EWK",
+    'isSignal' : 0,
+    'color'    : ROOT.kMagenta-10, # kViolet+10
+    'samples'  : ['WLLJJ_EWK']
+}
+groupPlot['ZZ']  = {
+    'nameHR' : "ZZ",
+    'isSignal' : 0,
+    'color'    : ROOT.kMagenta, # kViolet+10
+    'samples'  : ['ZZ']
+}
+groupPlot['VVV']  = {
+    'nameHR' : 'VVV',
+    'isSignal' : 0,
+    'color': ROOT.kSpring-9, # kGreen
+    'samples'  : ['VVV']
+}
+groupPlot['TTV']  = {
+    'nameHR' : 'TTV',
+    'isSignal' : 0,
+    'color': ROOT.kGray, # kGreen
+    'samples'  : ['TTV']
+}
+groupPlot['DPS']  = {
+    'nameHR' : 'DPS',
+    'isSignal' : 0,
+    'color': ROOT.kGray+5, # kGreen
+    'samples'  : ['DPS']
+}
+groupPlot['Vg']  = {
+    'nameHR' : "V#gamma",
+    'isSignal' : 0,
+    'color'    : ROOT.kCyan-7,   # kOrange + 10
+    'samples'  : ['Vg','VgS']
+}
+
+groupPlot['WW_strong']  = {
+    'nameHR' : "W^{#pm}W^{#pm} QCD",
+    'isSignal' : 0,
+    'color'    : ROOT.kViolet-4, # kViolet
+    'samples'  : ['WW_strong']
+}
+
+
+
+groupPlot['non-prompt']  = {
+                  'nameHR' : 'non-Prompt',
+                  'isSignal' : 0,
+                  'color': ROOT.kYellow-4,    # kYellow
+                  'samples'  : ['Fake_lep']
+              }
+'''
+groupPlot['TL_TT']  = {
+                  'nameHR' : "SSWW TTTL",
+                  'isSignal' : 0,
+                  'color'    : Azure+8, # kAzure+4
+                  'samples'  : ['TL_TT']
+              }
+groupPlot['LL']  = {
+                  'nameHR' : "SSWW LL",
+                  'isSignal' : 0,
+                  'color'    : Azure+4, # kAzure+4
+                  'samples'  : ['LL']
+              }
+'''
+groupPlot['WpWp_EWK']  = {
+                  'nameHR' : "W^{#pm}W^{#pm} EWK",
+                  'isSignal' : 0,
+                  'color'    : ROOT.kBlue-7, # kAzure+4
+                  'samples'  : ['WpWp_EWK']
+              }
+#plot = {}
+
+# keys here must match keys in samples.py
+#
+##Fake and prompt substraction
+plot['Fake_lep']  = {
+    'color': Yellow,    # kYellow
+    'isSignal' : 0,
+    'isData'   : 0,
+    'scale'    : 1.0
+}
+
+##Signal
+plot['WpWp_EWK']  = {
+    'color': Azure+4, # kAzure+4
+    'isSignal' : 0,
+    'isData'   : 0,
+    'scale'    : 1.0
+}
+'''
+plot['LL']  = {
+    'color': Azure+4, # kAzure+4
+    'isSignal' : 0,
+    'isData'   : 0,
+    'scale'    : 1.0
+}
+plot['TL_TT']  = {
+    'color': Azure+8, # kAzure+4
+    'isSignal' : 0,
+    'isData'   : 0,
+    'scale'    : 1.0
+}
+'''
+plot['WW_strong']  = {
+    'color': Violet, # kViolet
+    'isSignal' : 0,
+    'isData'   : 0,
+    'scale'    : 1.0
+}
+
+plot['Vg']  = {
+    'color': Orange+10, # kOrange+10
+    'isSignal' : 0,
+    'isData'   : 0,
+    'scale'    : 1.0
+}
+plot['VgS']  = {
+    'color': Orange+10, # kOrange+10
+    'isSignal' : 0,
+    'isData'   : 0,
+    'scale'    : 1.0
+}
+##Reducible Background
+##VV plot
+plot['ZZ']  = {
+    'color': Violet+10, # kViolet+10
+    'isSignal' : 0,
+    'isData'   : 0,
+    'scale'    : 1.0
+}
+plot['WLLJJ_QCD']  = {
+    'color': Violet+10, # kViolet+10
+    'isSignal' : 0,
+    'isData'   : 0,
+    'scale'    : 1.0
+}
+plot['WZTo2L2Q']  = {
+    'color': Violet+10, # kViolet+10
+    'isSignal' : 0,
+    'isData'   : 0,
+    'scale'    : 1.0
+}
+plot['WLLJJ_EWK']  = {
+    'color': Violet+10, # kViolet+10
+    'isSignal' : 0,
+    'isData'   : 0,
+    'scale'    : 1.0
+}
+plot['DPS']  = {
+    'color': Violet+10, # kViolet+10
+    'isSignal' : 0,
+    'isData'   : 0,
+    'scale'    : 1.0
+}
+##VVV
+plot['VVV']  = {
+    'color': Green, # kGreen
+    'isSignal' : 0,
+    'isData'   : 0,
+    'scale'    : 1.0
+}
+plot['TTV']  = {
+    'color': Green+10, # kGreen
+    'isSignal' : 0,
+    'isData'   : 0,
+    'scale'    : 1.0
+}
+##Data
+plot['DATA']  = {
+    'nameHR' : 'Data',
+    'color': 1 ,
+    'isSignal' : 0,
+    'isData'   : 1 ,
+    'isBlind'  : 0 ,
+    'scale'    : 1.0
+}
+
+# additional options
+legend['lumi'] = 'L = 59.74/fb'
+#legend['lumi'] = 'L = 137.19/fb'
+legend['sqrt'] = '#sqrt{s} = 13 TeV'
---
+++ Configurations/VBS/EFT/Full2018/variables.py
@@ -0,0 +1,222 @@
+# variables
+
+# variables = {}
+
+#'fold' : # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
+
+###########################
+##### LEPTON VARS #########
+###########################
+
+variables['nLepton'] =  {   'name': 'nLepton',
+                            'range': (7,0,7),
+                            'xaxis': '# leptons',
+                            'fold': 3
+}
+
+variables['mll']  = {   'name'  : 'mll',            #   variable name
+                        'range' : (30, 0. ,500),    #   variable range
+                        'xaxis' : 'mll [GeV]',      #   x axis name
+                        'fold'  : 3
+                        }
+
+## zoom around Z0
+# variables['mll_v2']  = {    'name'  : 'mll',              #   variable name
+#                             'range' : (100, 50 ,150),     #   variable range
+#                             'xaxis' : 'mll [GeV]',        #   x axis name
+#                             'fold'  : 3
+#                             }                        
+
+variables['pt1']  = {   'name'  : 'Alt$(Lepton_pt[0],-9999.)',
+                        'range' : (30,0.,400.),
+                        'xaxis' : 'p_{T} 1st lep [GeV]',
+                        'fold'  : 3
+                        }
+
+
+variables['pt2']  = {   'name'  : 'Alt$(Lepton_pt[1],-9999.)',
+                        'range' : (30,0.,300.),
+                        'xaxis' : 'p_{T} 2nd lep [GeV]',
+                        'fold'  : 3
+                        }      
+
+## eta leptons
+# variables['eta_lep1'] = {   'name' : 'Alt$(Lepton_eta[0],-9999.)',
+#                             'range': (20,-2.5,2.5),
+#                             'xaxis': 'eta lep1',
+#                             'fold' : 3
+#                             }
+
+# variables['eta_lep2'] = {   'name' : 'Alt$(Lepton_eta[1],-9999.)',
+#                             'range': (20,-2.5,2.5),
+#                             'xaxis': 'eta lep2',
+#                             'fold' : 3
+#                             }
+
+variables['detall']  = {  'name' : 'fabs(Alt$(Lepton_eta[0],-9999.)-Alt$(Lepton_eta[1],9999.))',
+                          'range': (20,0.0,5.0),
+                          'xaxis': 'deta ll',
+                          'fold' : 3
+                          }                            
+
+## phi leptons
+       
+# variables['phi_lep1'] = {   'name' : 'Alt$(Lepton_phi[0],-9999.)',
+#                             'range': (20,-3.141592,3.141592),
+#                             'xaxis': 'phi lep1',
+#                             'fold' : 3
+# }
+
+# variables['phi_lep2'] = {   'name' : 'Alt$(Lepton_phi[1],-9999.)',
+#                             'range': (20,-3.141592,3.141592),
+#                             'xaxis': 'phi lep2',
+#                             'fold' : 3  
+#                             }               
+
+variables['dphill'] = {     'name' : 'Alt$(dphill,-9999.)',
+                            'range': (10,0,3.141592),
+                            'xaxis': 'dphi ll',
+                            'fold' : 3    
+                            }                               
+
+
+###########################
+##### JETS   VARS #########
+###########################
+
+variables['nJet']  = {      'name'  : 'nCleanJet',
+                            'range' : (7,0,7),
+                            'xaxis' : '# Clean Jets',
+                            'fold'  : 3
+                            }
+
+variables['mjj']  = {  'name' : 'mjj',
+                       'range': (50,0.,2000.),
+                       'xaxis': 'mjj [GeV]',
+                       'fold' : 3
+                       }               
+# for low mjj control region
+variables['low_mjj']  = {   'name' : 'mjj',
+                            'range': (35,150,500.),
+                            'xaxis': 'mjj [GeV]',
+                            'fold' : 3
+                        }    
+
+variables['jetpt1']  = {   'name'  : 'Alt$(CleanJet_pt[0],-9999.)',
+                           'range' : (25,0.,500),
+                           'xaxis' : 'p_{T} 1st jet [GeV]',
+                           'fold'  : 3
+                           }
+
+variables['jetpt2']  = {   'name'  : 'Alt$(CleanJet_pt[1],-9999.)',
+                           'range' : (15,0.,300),
+                           'xaxis' : 'p_{T} 2nd jet [GeV]',
+                           'fold'  : 3
+                           }     
+
+# eta
+# variables['etaj1'] = {  'name' : 'Alt$(CleanJet_eta[0],-9999.)',
+#                         'range': (20,-5,5),
+#                         'xaxis': 'eta j1',
+#                         'fold' : 3
+#                         }
+
+# variables['etaj2'] = {  'name' : 'Alt$(CleanJet_eta[1],-9999.)',
+#                         'range': (20,-5,5),
+#                         'xaxis': 'eta j2',
+#                         'fold' : 3
+#                         }      
+
+variables['detajj']  = {  'name' : 'detajj',
+                          'range': (20,0.0,10.0),
+                          'xaxis': 'deta jj',
+                          'fold' : 3
+                          }
+
+# phi jets
+       
+# variables['phi_j1'] = {     'name' : 'Alt$(CleanJet_phi[0],-9999.)',
+#                             'range': (20,-3.141592,3.141592),
+#                             'xaxis': 'phi_j1',
+#                             'fold' : 3
+#                     }
+
+# variables['phi_j2'] = { 'name' : 'Alt$(CleanJet_phi[1],-9999.)',
+#                         'range': (20,-3.141592,3.141592),
+#                         'xaxis': 'phi_j2',
+#                         'fold' : 3  
+#                         }                             
+
+variables['dphijj']  = {    'name' : '(fabs(Alt$(CleanJet_phi[0],9999.) - Alt$(CleanJet_phi[1],-9999.)) <= 3.141592) * fabs(Alt$(CleanJet_phi[0],9999.) - Alt$(CleanJet_phi[1],-9999.)) + (fabs(Alt$(CleanJet_phi[0],9999.) - Alt$(CleanJet_phi[1],-9999.)) > 3.141592) * (2*3.141592 - (fabs(Alt$(CleanJet_phi[0],9999.) - Alt$(CleanJet_phi[1],-9999.))))' ,
+                            'range': (10,0.0,3.141592),
+                            'xaxis': 'dphi jj',
+                            'fold' : 3
+                        }                                                                                     
+                                                                                  
+
+###########################
+##### MET    VARS #########
+###########################
+
+variables['met']  = {   'name'  : 'MET_pt',         #  variable name
+                        'range' : (20,0,300),       #  variable range
+                        'xaxis' : 'met [GeV]',      #  x axis name
+                        'fold'  : 3
+                    }
+
+# variables['MET_significance']  = {  'name'  : 'MET_significance',   
+#                                     'range' : (20,0.,1.),           
+#                                     'xaxis' : 'MET_significance',   
+#                                     'fold'  : 3
+#                     }
+
+###########################
+##### bTag Variables ######
+###########################
+
+variables['Jet_btagDeepB_0']  = {   'name'  : 'Jet_btagDeepB[CleanJet_jetIdx[0]]',   
+                                    'range' : (20,0,1),        
+                                    'xaxis' : 'Jet_btagDeepB_0',     
+                                    'fold'  : 3
+                                }
+
+variables['Jet_btagDeepB_1']  = {   'name'  : 'Jet_btagDeepB[CleanJet_jetIdx[1]]',   
+                                    'range' : (20,0,1),        
+                                    'xaxis' : 'Jet_btagDeepB_1',     
+                                    'fold'  : 3
+                                }
+
+# one only for the first two jets. It gives the btag variable for the most central of the first two CleanJets
+variables['my_btag_var']  = {   'name'  : '(fabs(CleanJet_eta[0]) <= fabs(CleanJet_eta[1])) * Jet_btagDeepB[CleanJet_jetIdx[0]] + (fabs(CleanJet_eta[0]) > fabs(CleanJet_eta[1])) * Jet_btagDeepB[CleanJet_jetIdx[1]]',
+                                'range' : (20,0,1),        
+                                'xaxis' : 'central_jet_btag_var',     
+                                'fold'  : 3
+                            }
+
+
+#############################
+#### Zeppenfeld Variable ####
+#############################                     
+                        
+variables['Zlep1']  = {  'name': 'zlep1',
+                         'range': (30,-1.5,1.5),
+                         'xaxis': 'Z^{lep}_{1}',
+                         'fold': 3
+                         }
+
+variables['Zlep2']  = {  'name': 'zlep2',
+                         'range': (30,-1.5,1.5),
+                         'xaxis': 'Z^{lep}_{2}',
+                         'fold': 3
+                         }
+
+###############################
+### control var for pt veto ###
+###############################
+
+# variables['pt3']  = {   'name'  : 'Alt$(Lepton_pt[2],-9999.)',
+#                         'range' : (25,0.,100.),
+#                         'xaxis' : 'p_{T} 3rd lep [GeV]',
+#                         'fold'  : 3
+#                         }  
+
