19aa6ac1ce10443c44682e8faa0cf8b424c03689
dbrambilla13 <daniele.bram@gmail.com>
2020-02-28 12:48:27 +0100

    WZ cr configuration update

---
+++ Configurations/VBS/EFT/Full2018_cr_WZ/samples.py
@@ -0,0 +1,294 @@
+import os
+import inspect
+
+# FIX THIS in dependence of where this file is stored, in order to correctly locate the macros in Differential and Patches folder
+# check also in samples.py
+configurations = os.path.realpath(inspect.getfile(inspect.currentframe())) # this file
+configurations = os.path.dirname(configurations) # 2018 folder
+configurations = os.path.dirname(configurations) # EFT
+configurations = os.path.dirname(configurations) # VBS, Differential & Patches level
+configurations = os.path.dirname(configurations) # Configurations level
+
+from LatinoAnalysis.Tools.commonTools import getSampleFiles, getBaseW, addSampleWeight
+
+def nanoGetSampleFiles(inputDir, sample):
+    try:
+        if _samples_noload:
+            return []
+    except NameError:
+        pass
+
+    return getSampleFiles(inputDir, sample, True, 'nanoLatino_')
+
+# samples
+
+try:
+    len(samples)
+except NameError:
+    import collections
+    samples = collections.OrderedDict()
+
+# mcDirectory = '/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/Autumn18_102X_nAODv6_Full2018v6/MCl1loose2018v6__MCCorr2018v6__l2loose__l2tightOR2018v6/'
+mcDirectory = '/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/Autumn18_102X_nAODv5_Full2018v5/MCl1loose2018v5__MCCorr2018v5__l2loose__l2tightOR2018v5'
+#mcDir_private='/eos/user/j/jixiao/HWWnano3/Autumn18_102X_nAODv5_Full2018v5/MCl1loose2018v5__MCCorr2018v5__l2loose__l2tightOR2018v5/'
+mcDir_private = '/afs/cern.ch/work/j/jixiao/public/MCl1loose2018v5__MCCorr2018v5__l2loose__l2tightOR2018v5/'
+
+# treeBaseDir = '/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano'
+# mcProduction = 'Autumn18_102X_nAODv6_Full2018v6'
+# mcSteps = 'MCl1loose2018v6__MCCorr2018v6__l2loose__l2tightOR2018v6{var}'
+# def makeMCDirectory(var=''):
+#     if var:
+#         return os.path.join(treeBaseDir, mcProduction, mcSteps.format(var='__' + var))
+#     else:
+#         return os.path.join(treeBaseDir, mcProduction, mcSteps.format(var=''))
+
+################################################
+############ BASIC MC WEIGHTS ##################
+################################################
+mcCommonWeightNoMatch = 'SFweight'
+mcCommonWeight = 'SFweight*PromptGenLepMatch3l'
+################################################
+############ DATA DECLARATION ##################
+################################################
+DataRun = [
+    ['A','Run2018A-Nano25Oct2019-v1'] ,
+    ['B','Run2018B-Nano25Oct2019-v1'] ,
+    ['C','Run2018C-Nano25Oct2019-v1'] ,
+    ['D','Run2018D-Nano25Oct2019_ver2-v1'] ,
+]
+DataSets = ['MuonEG','DoubleMuon','SingleMuon','EGamma']
+
+DataTrig = {
+    'MuonEG'         : 'Trigger_ElMu' ,
+    'DoubleMuon'     : '!Trigger_ElMu && Trigger_dblMu' ,
+    'SingleMuon'     : '!Trigger_ElMu && !Trigger_dblMu && Trigger_sngMu' ,
+    'EGamma'         : '!Trigger_ElMu && !Trigger_dblMu && !Trigger_sngMu && (Trigger_sngEl || Trigger_dblEl)' ,
+}
+###########################################
+############  Reducible Bkg  ##############
+###########################################
+######## Vg ########
+
+files = nanoGetSampleFiles(mcDirectory, 'WGJJ') + \
+        nanoGetSampleFiles(mcDirectory, 'Zg')
+
+samples['Vg'] = {
+    'name': files,
+    'weight': mcCommonWeightNoMatch + '*!(Gen_ZGstar_mass > 0)',
+    'FilesPerJob': 10
+}
+addSampleWeight(samples, 'Vg', 'Zg', '0.448')
+
+######## VgS ########
+files = nanoGetSampleFiles(mcDirectory, 'Wg_MADGRAPHMLM') + \
+        nanoGetSampleFiles(mcDirectory, 'Zg') + \
+        nanoGetSampleFiles(mcDirectory, 'WZTo3LNu_mllmin01')
+
+samples['VgS'] = {
+    'name': files,
+    'weight': mcCommonWeight + ' * (gstarLow * 0.94 + gstarHigh * 1.14)',
+    'FilesPerJob': 15,
+    'subsamples': {
+        'L': 'gstarLow',
+        'H': 'gstarHigh'
+    }
+}
+addSampleWeight(samples, 'VgS', 'Wg_MADGRAPHMLM', '(Gen_ZGstar_mass > 0 && Gen_ZGstar_mass < 0.1)')
+addSampleWeight(samples, 'VgS', 'Zg', '(Gen_ZGstar_mass > 0)*0.448')
+addSampleWeight(samples, 'VgS', 'WZTo3LNu_mllmin01', '(Gen_ZGstar_mass > 0.1 && Gen_ZGstar_mass<4)')
+files = nanoGetSampleFiles(mcDirectory, 'Zg') + \
+        nanoGetSampleFiles(mcDirectory, 'WGJJ') + \
+        nanoGetSampleFiles(mcDirectory, 'WZTo3LNu_mllmin01')
+samples['VgS1'] = {
+    'name': files,
+    'weight': mcCommonWeight + ' * (gstarLow * 0.94 + gstarHigh * 1.14)',
+    'FilesPerJob': 4,
+    'subsamples': {
+        'L': 'gstarLow',
+        'H': 'gstarHigh'
+    }
+}
+addSampleWeight(samples, 'VgS1', 'WGJJ', '(Gen_ZGstar_mass > 0 && Gen_ZGstar_mass < 0.1)')
+addSampleWeight(samples, 'VgS1', 'Zg', '(Gen_ZGstar_mass > 0)*0.448')
+addSampleWeight(samples, 'VgS1', 'WZTo3LNu_mllmin01', '(Gen_ZGstar_mass > 0.1 && Gen_ZGstar_mass<4)')
+
+files = nanoGetSampleFiles(mcDirectory, 'Zg') + \
+        nanoGetSampleFiles(mcDirectory, 'WGJJ')
+samples['VgS2'] = {
+    'name': files,
+    'weight': mcCommonWeight + ' * (gstarLow * 0.94 + gstarHigh * 1.14)',
+    'FilesPerJob': 4,
+    'subsamples': {
+        'L': 'gstarLow',
+        'H': 'gstarHigh'
+    }
+}
+addSampleWeight(samples, 'VgS2', 'WGJJ', '(Gen_ZGstar_mass > 0 && Gen_ZGstar_mass < 0.1)')
+addSampleWeight(samples, 'VgS2', 'Zg', '(Gen_ZGstar_mass > 0)*0.448')
+######### VV #########
+files = nanoGetSampleFiles(mcDirectory, 'ZZTo2L2Nu_ext2') + \
+        nanoGetSampleFiles(mcDirectory, 'ZZTo2L2Q') + \
+        nanoGetSampleFiles(mcDirectory, 'ZZTo4L_ext2')
+#nanoGetSampleFiles(mcDirectory, 'ZZTo2L2Nu_ext2') + \
+#nanoGetSampleFiles(mcDirectory, 'ZZTo4L_ext2')
+
+samples['ZZ'] = {
+    'name': files,
+    'weight': mcCommonWeight+ '*1.11',
+    'FilesPerJob': 4
+}
+files = nanoGetSampleFiles(mcDirectory, 'WZTo2L2Q')
+samples['WZTo2L2Q'] = {
+    'name': files,
+    'weight': mcCommonWeight+ '*1.11',
+    'FilesPerJob': 4
+}
+files = nanoGetSampleFiles(mcDirectory, 'WLLJJToLNu_M-50_QCD_0Jet') + \
+        nanoGetSampleFiles(mcDirectory, 'WLLJJToLNu_M-50_QCD_1Jet') + \
+        nanoGetSampleFiles(mcDirectory, 'WLLJJToLNu_M-50_QCD_2Jet') + \
+        nanoGetSampleFiles(mcDirectory, 'WLLJJToLNu_M-50_QCD_3Jet') + \
+        nanoGetSampleFiles(mcDirectory, 'WLLJJToLNu_M-4To50_QCD_0Jet') + \
+        nanoGetSampleFiles(mcDirectory, 'WLLJJToLNu_M-4To50_QCD_1Jet') + \
+        nanoGetSampleFiles(mcDirectory, 'WLLJJToLNu_M-4To50_QCD_2Jet') + \
+        nanoGetSampleFiles(mcDirectory, 'WLLJJToLNu_M-4To50_QCD_3Jet')
+
+samples['WZ_QCD'] = {
+    'name': files,
+    'weight': mcCommonWeight+'*1.2',
+    'FilesPerJob': 4
+}
+files = nanoGetSampleFiles(mcDirectory, 'WZTo3LNu_powheg')
+samples['WZ_QCD_powheg'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 4
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'WZTo3LNu_ext1')
+samples['WZ_QCD_AMCNLO'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 4
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'WZTo3LNu_mllmin01')
+samples['WZ_QCD_mllmin01'] = {
+    'name': files,
+    'weight': mcCommonWeight+'*(Gen_ZGstar_mass > 4)',
+    'FilesPerJob': 4
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'WLLJJ_WToLNu_EWK')# + \
+samples['WZ_EWK'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 4
+}
+########## VVV #########
+
+files = nanoGetSampleFiles(mcDirectory, 'ZZZ') + \
+        nanoGetSampleFiles(mcDirectory, 'WZZ') + \
+        nanoGetSampleFiles(mcDirectory, 'WWZ') + \
+        nanoGetSampleFiles(mcDirectory, 'WWW')
+#+ nanoGetSampleFiles(mcDirectory, 'WWG'), #should this be included? or is it already taken into account in the WW sample?
+
+samples['VVV'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 4
+}
+
+########## TTV #########
+
+files = nanoGetSampleFiles(mcDirectory, 'TTWJetsToLNu') + \
+        nanoGetSampleFiles(mcDirectory, 'TTZToLLNuNu_M-10') + \
+        nanoGetSampleFiles(mcDirectory, 'tZq_ll')
+#+ nanoGetSampleFiles(mcDirectory, 'WWG'), #should this be included? or is it already taken into account in the WW sample?
+
+samples['TTV'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 4
+}
+########## DPS #########
+files = nanoGetSampleFiles(mcDirectory, 'WWTo2L2Nu_DoubleScattering')
+#+ nanoGetSampleFiles(mcDirectory, 'WWG'), #should this be included? or is it already taken into account in the WW sample?
+
+samples['DPS'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 4
+}
+###########################################
+#######  IRREDUCIBLE BACKGROUNDS  #########
+###########################################
+files = nanoGetSampleFiles(mcDirectory, 'WpWpJJ_QCD')
+#+ nanoGetSampleFiles(mcDirectory, 'WWG'), #should this be included? or is it already taken into account in the WW sample?
+
+samples['WpWp_QCD'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 4
+}
+###########################################
+#############   SIGNALS  ##################
+###########################################
+files = nanoGetSampleFiles(mcDirectory, 'WpWpJJ_EWK_madgraph')
+#+ nanoGetSampleFiles(mcDirectory, 'WWG'), #should this be included? or is it already taken into account in the WW sample?
+
+samples['WpWp_EWK'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 4
+}
+
+files = nanoGetSampleFiles(mcDir_private, 'SSWW_SM')
+samples['sm'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 4
+}
+files = nanoGetSampleFiles(mcDir_private, 'SSWW_INT')
+samples['linear'] = {
+    'name': files,
+    'weight': mcCommonWeight+'*(1/0.3)', # 1/0.3 is used to normalize to the cross section to Cw=1, current samples are generated with Cw=0.3
+    'FilesPerJob': 4
+}
+files = nanoGetSampleFiles(mcDir_private, 'SSWW_BSM')
+samples['quadratic'] = {
+    'name': files,
+    'weight': mcCommonWeight+'*(1/0.09)', # 1/0.09 is used to normalize to the cross section to Cw=1, current samples are generated with Cw=0.3
+    'FilesPerJob': 4
+}
+###########################################
+################## FAKE ###################
+###########################################
+#1389 files
+samples['Fake_lep'] = {
+    'name': [],
+    'weight': 'METFilter_DATA*fakeW',
+    'weights': [],
+    'isData': ['all'],
+    'FilesPerJob': 17
+}
+
+for _, sd in DataRun:
+    for pd in DataSets:
+        files = nanoGetSampleFiles('/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/Run2018_102X_nAODv6_Full2018v6/DATAl1loose2018v6__l2loose__fakeW/', pd + '_' + sd)
+        samples['Fake_lep']['name'].extend(files)
+        samples['Fake_lep']['weights'].extend([DataTrig[pd]] * len(files))
+###########################################
+################## DATA ###################
+samples['DATA'] = {
+    'name': [],
+    'weight': 'METFilter_DATA*LepWPCut',
+    'weights': [],
+    'isData': ['all'],
+    'FilesPerJob': 17
+}
+
+for _, sd in DataRun:
+    for pd in DataSets:
+        files = nanoGetSampleFiles('/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/Run2018_102X_nAODv6_Full2018v6/DATAl1loose2018v6__l2loose__l2tightOR2018v6/', pd + '_' + sd)
+        samples['DATA']['name'].extend(files)
+        samples['DATA']['weights'].extend([DataTrig[pd]] * len(files))
