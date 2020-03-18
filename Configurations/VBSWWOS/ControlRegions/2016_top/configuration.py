c48c7a2af5e2047ce05595846f52540978fb8720
Flavia Cetorelli <f.cetorelli@campus.unimib.it>
2020-03-18 09:25:35 +0100

    Top control region and signal region

---
+++ Configurations/VBSWWOS/ControlRegions/2016_top/configuration.py
@@ -0,0 +1,35 @@
+# example of configuration file
+
+tag = '2016_top'
+outputDir = 'rootFile'
+treeName = 'Events'
+
+# luminosity to normalize to
+lumi = 35.9
+
+# file with TTree aliases
+aliasesFile = 'aliases.py'
+
+# file with list of variables
+variablesFile = 'variables.py' 
+
+# file with list of cuts
+cutsFile = 'cuts.py' 
+
+# file with list of samples
+samplesFile = 'samples.py' 
+
+# file with list of plot
+plotFile = 'plot.py'
+
+# nuisances file for mkDatacards and for mkShape
+nuisancesFile = 'nuisances.py'
+# structure file for datacard
+structureFile = 'structure.py'
+
+# used by mkPlot to define output directory for plots
+# different from "outputDir" to do things more tidy
+outputDirPlots = '/eos/user/f/fcetorel/www/VBS_OS/test/2016/top_v2'
+# used by mkDatacards to define output directory for datacards
+outputDirDatacard = '/afs/cern.ch/work/f/fcetorel/private/work2/VBS_OS/CMSSW_10_2_15_patch2/src/PlotsConfigurations/Configurations/VBSOS/ControlRegions/2016_top/'
+
---
+++ Configurations/VBSWWOS/SignalRegions/2018/structure.py
@@ -0,0 +1,91 @@
+'''structure['NAME']  = { # NAME should match the names in samples.py
+     'isSignal' : 0, # 0/1 with obvious meaning
+     'isData'   : 0, # 0/1 with obvious meaning 
+     'removeFromCuts' : []  #OPTIONAL list of cuts that should not be run for this sample (default is empty)
+}
+'''
+structure['DY']  = { 
+     'isSignal' : 0,
+     'isData'   : 0
+}
+
+structure['top']  = { 
+     'isSignal' : 0,
+     'isData'   : 0
+}
+
+structure['WWqcd']  = { 
+     'isSignal' : 0,
+     'isData'   : 0
+}
+
+structure['WW']  = { 
+     'isSignal' : 0,
+     'isData'   : 0
+}
+
+structure['VVV']  = { 
+     'isSignal' : 0,
+     'isData'   : 0
+}
+
+structure['VZ']  = { 
+     'isSignal' : 0,
+     'isData'   : 0
+}
+
+structure['Vg']  = { 
+     'isSignal' : 0,
+     'isData'   : 0
+}
+
+structure['VgS_H']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0 
+              }
+structure['VgS_L']  = {  
+                  'isSignal' : 0,  
+                  'isData'   : 0 
+              }
+
+structure['ggWW']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+              }
+structure['Fake_em']  = { 
+                 'isSignal' : 0,
+                 'isData'   : 0 
+             }
+structure['Fake_me']  = {  
+                 'isSignal' : 0,
+                 'isData'   : 0 
+             }
+structure['Fake_ee']  = { 
+                 'isSignal' : 0,
+                 'isData'   : 0 
+             }
+structure['Fake_mm']  = { 
+                 'isSignal' : 0,
+                 'isData'   : 0 
+             }
+
+structure['qqH_hww'] = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+
+
+structure['ggH_hww'] = {
+                  'isSignal' : 0,
+                  'isData'   : 0
+                  }
+structure['DATA']  = {   
+                'isSignal' : 0,
+               'isData'   : 1 
+          }
+structure['WWewk']  = {
+                  'isSignal' : 1,
+                  'isData'   : 0 ,
+              }
+
+
