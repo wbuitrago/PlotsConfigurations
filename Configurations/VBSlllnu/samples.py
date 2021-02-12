8b6c42830bee3c51ddf0fae7b3205b25a249014a
Elena Vernazza <e.vernazza1@campus.unimib.it>
2021-02-12 09:59:32 +0100

    LI and QU separated

--- Configurations/VBSlllnu/samples.py
+++ Configurations/VBSlllnu/samples.py
@@ -31,6 +31,25 @@ except NameError:
 mcCommonWeightNoMatch = 'SFweight'
 mcCommonWeight = 'SFweight*PromptGenLepMatch3l'
 
+mcDirectory_off = '/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/Autumn18_102X_nAODv7_Full2018v7/MCl1loose2018v7__MCCorr2018v7__l2loose__l2tightOR2018v7'
+mcProduction_off = 'Autumn18_102X_nAODv7_Full2018v7'
+treeBaseDir_off = '/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano'
+mcSteps_off = 'MCl1loose2018v7__MCCorr2018v7__l2loose__l2tightOR2018v7{var}'
+
+def makeMCDirectory_off(var=''):
+    if var:
+        return os.path.join(treeBaseDir_off, mcProduction_off, mcSteps_off.format(var='__' + var))
+    else:
+        return os.path.join(treeBaseDir_off, mcProduction_off, mcSteps_off.format(var='')) 
+
+files = nanoGetSampleFiles(mcDirectory_off, 'WLLJJ_WToLNu_EWK')
+samples['WZ_EWK'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 4
+}
+
+
 mcDirectory = '/eos/user/e/evernazz/nanoAOD_PostProc_ntuple/Autumn18_102X_nAODv7_Full2018v7/MCl1loose2018v7__MCCorr2018v7__l2loose__l2tightOR2018v7'
 treeBaseDir = '/eos/user/e/evernazz/nanoAOD_PostProc_ntuple'
 mcProduction = 'Autumn18_102X_nAODv7_Full2018v7'
@@ -46,6 +65,20 @@ files = nanoGetSampleFiles(mcDirectory, 'WZeu_SM')
 samples['SM'] = {
     'name': files,
     'weight': mcCommonWeight,
+    'FilesPerJob': 5,
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'WZeu_cqq11_LI')
+samples['cqq11_LI'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 5
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'WZeu_cqq11_QU')
+samples['cqq11_QU'] = {
+    'name': files,
+    'weight': mcCommonWeight,
     'FilesPerJob': 5
 }
 
@@ -58,6 +91,76 @@ samples['cqq1_LI'] = {
 
 files = nanoGetSampleFiles(mcDirectory, 'WZeu_cqq1_QU')
 samples['cqq1_QU'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 5
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'WZeu_cqq31_LI')
+samples['cqq31_LI'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 5
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'WZeu_cqq31_QU')
+samples['cqq31_QU'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 5
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'WZeu_cqq3_LI')
+samples['cqq3_LI'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 5
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'WZeu_cqq3_QU')
+samples['cqq3_QU'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 5
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'WZeu_cHl3_LI')
+samples['cHl3_LI'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 5
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'WZeu_cHl3_QU')
+samples['cHl3_QU'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 5
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'WZeu_cHq3_LI')
+samples['cHq3_LI'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 5
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'WZeu_cHq3_QU')
+samples['cHq3_QU'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 5
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'WZeu_cll1_LI')
+samples['cll1_LI'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 5
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'WZeu_cll1_QU')
+samples['cll1_QU'] = {
     'name': files,
     'weight': mcCommonWeight,
     'FilesPerJob': 5
--- Configurations/VBSlllnu/variables.py
+++ Configurations/VBSlllnu/variables.py
@@ -19,12 +19,12 @@ variables['nJet']={'name':'Sum$(CleanJet_pt>30)',
                    'fold':3
                    }
 
-variables['nLepton']={
-    'name':'1*(Alt$(Lepton_pt[0],0.)>10) + 1*(Alt$(Lepton_pt[1],0.)>10) + 1*(Alt$(Lepton_pt[2],0.)>10)+ 1*(Alt$(Lepton_pt[3],0.)>10) + 1*(Alt$(Lepton_pt[4],0.)>10)',
-    'range':(5,0,5),
-    'xaxis':'# leptons',
-    'fold':3
-}
+#variables['nLepton']={
+#    'name':'1*(Alt$(Lepton_pt[0],0.)>10) + 1*(Alt$(Lepton_pt[1],0.)>10) + 1*(Alt$(Lepton_pt[2],0.)>10)+ 1*(Alt$(Lepton_pt[3],0.)>10) + 1*(Alt$(Lepton_pt[4],0.)>10)',
+#    'range':(5,0,5),
+#    'xaxis':'# leptons',
+#    'fold':3
+#}
 variables['mll']={'name':'mll',  # variable name
                   'range':(12,20.,500),  # variable range
                   'xaxis':'mll [GeV]',  # x axis name
@@ -35,70 +35,70 @@ variables['mll']={'name':'mll',  # variable name
 #                            'xaxis' : 'mll v1 [GeV]',      #   x axis name
 #                            'fold'  : 3
 #                            }
-variables['mll_v2']={'name':'mll',  # variable name
-                     'range':([20,60,80,110,140,240,380,500],),  # variable range
-                     'xaxis':'mll v1 [GeV]',  # x axis name
-                     'fold':3
-                     }
-variables['pt1']={'name':'Alt$(Lepton_pt[0],-9999.)',
+#variables['mll_v2']={'name':'mll',  # variable name
+#                     'range':([20,60,80,110,140,240,380,500],),  # variable range
+#                     'xaxis':'mll v1 [GeV]',  # x axis name
+#                     'fold':3
+#                     }
+variables['ptl1']={'name':'Alt$(Lepton_pt[0],-9999.)',
                   'range':(10,30.,300.),
                   'xaxis':'p_{T} 1st lep [GeV]',
                   'fold':3
                   }
-variables['pt1_v1']={'name':'Alt$(Lepton_pt[0],-9999.)',
-                     'range':(15,30.,510.),
-                     'xaxis':'p_{T} 1st lep [GeV]',
-                     'fold':3
-                     }
+#variables['pt1_v1']={'name':'Alt$(Lepton_pt[0],-9999.)',
+#                     'range':(15,30.,510.),
+#                     'xaxis':'p_{T} 1st lep [GeV]',
+#                     'fold':3
+#                     }
 
-variables['pt2']={'name':'Alt$(Lepton_pt[1],-9999.)',
-                  'range':(6,30.,210.),
+variables['ptl2']={'name':'Alt$(Lepton_pt[1],-9999.)',
+                  'range':(16,30.,210.),
                   'xaxis':'p_{T} 2nd lep [GeV]',
                   'fold':3
                   }
-variables['pt2_v2']={'name':'Alt$(Lepton_pt[1],-9999.)',
-                     'range':(10,30.,300.),
-                     'xaxis':'p_{T} 2nd lep [GeV]',
-                     'fold':3
-                     }
+#variables['pt2_v2']={'name':'Alt$(Lepton_pt[1],-9999.)',
+#                     'range':(50,30.,300.),
+#                     'xaxis':'p_{T} 2nd lep [GeV]',
+#                     'fold':3
+#                     }
 ## eta leptons
-variables['eta_lep1'] = {'name' : 'Alt$(Lepton_eta[0],-9999.)',
+variables['etal1'] = {'name' : 'Alt$(Lepton_eta[0],-9999.)',
                          'range': (10,-2.5,2.5),
                          'xaxis': 'eta lep1',
                          'fold' : 3
                          }
 
-variables['eta_lep2'] = {   'name' : 'Alt$(Lepton_eta[1],-9999.)',
+variables['etal2'] = {   'name' : 'Alt$(Lepton_eta[1],-9999.)',
                             'range': (10,-2.5,2.5),
                             'xaxis': 'eta lep2',
                             'fold' : 3
                             }
 
-variables['detall']={'name':'detall',
-                     'range':(10,0.0,3.5),
-                     'xaxis':'deta ll',
-                     'fold':3
-                     }
+#variables['detall']={'name':'detall',
+#                     'range':(10,0.0,3.5),
+#                     'xaxis':'deta ll',
+#                     'fold':3
+#                     }
 
 ## phi leptons
 
-variables['phi_lep1'] = {   'name' : 'Alt$(Lepton_phi[0],-9999.)',
-                            'range': (10,-3.141592,3.141592),
-                            'xaxis': 'phi lep1',
-                            'fold' : 3
-                            }
+#variables['phi_lep1'] = {   'name' : 'Alt$(Lepton_phi[0],-9999.)',
+#                            'range': (10,-3.141592,3.141592),
+#                            'xaxis': 'phi lep1',
+#                            'fold' : 3
+#                            }
 
-variables['phi_lep2'] = {   'name' : 'Alt$(Lepton_phi[1],-9999.)',
-                            'range': (10,-3.141592,3.141592),
-                            'xaxis': 'phi lep2',
-                            'fold' : 3
-                            }
+#variables['phi_lep2'] = {   'name' : 'Alt$(Lepton_phi[1],-9999.)',
+#                            'range': (10,-3.141592,3.141592),
+#                            'xaxis': 'phi lep2',
+#                            'fold' : 3
+#                            }
 
-variables['dphill']={'name':'dphill',
-                     'range':(10,0,3.141592),
-                     'xaxis':'dphi ll',
-                     'fold':3
-                     }
+#variables['dphill']={'name':'dphill',
+#                     'range':(10,0,3.141592),
+#                     'xaxis':'dphi ll',
+#                     'fold':3
+#                     }
 
 ###########################
 ##### JETS   VARS #########
@@ -116,16 +116,16 @@ variables['mjj']={'name':'mjj',
                   'fold':3
                   }
 
-variables['mjj_v1']={'name':'mjj',
-                     'range':([500,800,1100,1500,2000,3000],),
-                     'xaxis':'mjj v1[GeV]',
-                     'fold':3
-                     }
-variables['mjj_v2']={'name':'mjj',
-                     'range':([500, 1000, 1500,2000, 3000],),
-                     'xaxis':'mjj v1[GeV]',
-                     'fold':3
-                     }
+#variables['mjj_v1']={'name':'mjj',
+#                     'range':([500,800,1100,1500,2000,3000],),
+#                     'xaxis':'mjj v1[GeV]',
+#                     'fold':3
+#                     }
+#variables['mjj_v2']={'name':'mjj',
+#                     'range':([500, 1000, 1500,2000, 3000],),
+#                     'xaxis':'mjj v1[GeV]',
+#                     'fold':3
+#                     }
 # for low mjj control region
 # variables['low_mjj']  = {   'name' : 'mjj',
 #                             'range': (10,150,500.),
@@ -133,19 +133,19 @@ variables['mjj_v2']={'name':'mjj',
 #                             'fold' : 3
 #                         }
 
-variables['jetpt1']={'name':'Alt$(CleanJet_pt[0],-9999.)',
+variables['ptj1']={'name':'Alt$(CleanJet_pt[0],-9999.)',
                      'range':(10,30,300),
                      'xaxis':'p_{T} 1st jet [GeV]',
                      'fold':3
                      }
 
-variables['jetpt1_v1']={'name':'Alt$(CleanJet_pt[0],-9999.)',
-                        'range':(12,30,510),
-                        'xaxis':'p_{T} 1st jet v1[GeV]',
-                        'fold':3
-                        }
+#variables['jetpt1_v1']={'name':'Alt$(CleanJet_pt[0],-9999.)',
+#                        'range':(12,30,510),
+#                        'xaxis':'p_{T} 1st jet v1[GeV]',
+#                        'fold':3
+#                        }
 
-variables['jetpt2']={'name':'Alt$(CleanJet_pt[1],-9999.)',
+variables['ptj2']={'name':'Alt$(CleanJet_pt[1],-9999.)',
                      'range':(10,30,250),
                      'xaxis':'p_{T} 2nd jet [GeV]',
                      'fold':3
@@ -161,27 +161,27 @@ variables['etaj2'] = {  'name' : 'Alt$(CleanJet_eta[1],-9999.)',
                         'range': (10,-5,5),
                         'xaxis': 'eta j2',
                         'fold' : 3
-                        }
-
-variables['detajj']={'name':'detajj',
-                     'range':(5,2.5,7.5),
-                     'xaxis':'deta jj',
-                     'fold':3
                      }
 
-variables['detajj_v1']={'name':'detajj',
-                        'range':([2.5,3.5,5.0,6.5,9.0],),
-                        'xaxis':'deta jj v1',
-                        'fold':3
-                        }
+variables['detajj'] = { 'name'  : 'detajj',
+                        'range' : (5,2.5,7.5),
+                        'xaxis' : 'deta jj',
+                        'fold'  : 3
+                      }
+
+#variables['detajj_v1']={'name':'detajj',
+#                        'range':([2.5,3.5,5.0,6.5,9.0],),
+#                        'xaxis':'deta jj v1',
+#                        'fold':3
+#                        }
 
 # phi jets
 
-variables['phi_j1'] = {     'name' : 'Alt$(CleanJet_phi[0],-9999.)',
-                            'range': (10,-3.141592,3.141592),
-                            'xaxis': 'phi_j1',
-                            'fold' : 3
-                            }
+variables['phi_j1'] = { 'name' : 'Alt$(CleanJet_phi[0],-9999.)',
+                        'range': (10,-3.141592,3.141592),
+                        'xaxis': 'phi_j1',
+                        'fold' : 3
+                        }
 
 variables['phi_j2'] = { 'name' : 'Alt$(CleanJet_phi[1],-9999.)',
                         'range': (10,-3.141592,3.141592),
@@ -231,33 +231,33 @@ variables['met']={'name':'MET_pt',  # variable name
 #### Zeppenfeld Variable ####
 #############################
 
-variables['Zlep1']  = {  'name': 'zlep1',
-                         'range': (10,-0.75,0.75),
-                         'xaxis': 'Z^{lep}_{1}',
-                         'fold': 3
-                         }
-variables['Zlep2']  = {  'name': 'zlep2',
-                         'range': (10,-0.75,0.75),
-                         'xaxis': 'Z^{lep}_{2}',
-                         'fold': 3
-                         }
-variables['mllVSmjj']={'name':'mll:mjj',
-                       'range':([20,120,220,320],[500,1000,1800,2500],),
-                       'xaxis' : 'm_{ll} : m_{jj}',
-                       'fold': 3,
-                       'doWeight': 1,
-                       'binX': 3,
-                       'binY': 3
-                       }
+#variables['Zlep1']  = {  'name': 'zlep1',
+#                         'range': (10,-0.75,0.75),
+#                         'xaxis': 'Z^{lep}_{1}',
+#                         'fold': 3
+#                         }
+#variables['Zlep2']  = {  'name': 'zlep2',
+#                         'range': (10,-0.75,0.75),
+#                         'xaxis': 'Z^{lep}_{2}',
+#                         'fold': 3
+#                         }
+#variables['mllVSmjj']={'name':'mll:mjj',
+#                       'range':([20,120,220,320],[500,1000,1800,2500],),
+#                       'xaxis' : 'm_{ll} : m_{jj}',
+#                       'fold': 3,
+#                       'doWeight': 1,
+#                       'binX': 3,
+#                       'binY': 3
+#                       }
 
-variables['mllVSmjj_v2']={'name':'mll:mjj',
-                          'range':([20, 80, 140, 240 ,500],[500, 650, 800, 1000, 1200, 1500, 1800, 2300,3000],),
-                          'xaxis' : 'm_{ll} : m_{jj}',
-                          'fold': 3,
-                          'doWeight': 1,
-                          'binX': 4,
-                          'binY': 8
-                          }
+#variables['mllVSmjj_v2']={'name':'mll:mjj',
+#                          'range':([20, 80, 140, 240 ,500],[500, 650, 800, 1000, 1200, 1500, 1800, 2300,3000],),
+#                          'xaxis' : 'm_{ll} : m_{jj}',
+#                          'fold': 3,
+#                          'doWeight': 1,
+#                          'binX': 4,
+#                          'binY': 8
+#                          }
 #variables['DNNScore_WW'] = {
 #    'name': 'DNNScore_WW',
 #    'range': (20,0.,1),
@@ -320,11 +320,11 @@ variables['mllVSmjj_v2']={'name':'mll:mjj',
 #    'fold' : 3 ,
 #}
 
-variables['mjjVSdetajj']={'name':'mjj:detajj',
-                          'range':([2.5, 4.0, 5.0, 7.0],[500, 1000, 1500,2000, 3000],),
-                          'xaxis' : 'm_{jj} : |#Delta#eta_{jj}|',
-                          'fold': 3,
-                          }
+#variables['mjjVSdetajj']={'name':'mjj:detajj',
+#                          'range':([2.5, 4.0, 5.0, 7.0],[500, 1000, 1500,2000, 3000],),
+#                          'xaxis' : 'm_{jj} : |#Delta#eta_{jj}|',
+#                          'fold': 3,
+#                          }
 #variables['DNNScore_VBS1'] = {
 #    'name': 'DNNScore_VBS',            #   variable name
 #    'range' : ([0,0.454,0.588,0.669,0.740,1],),            #   variable range
