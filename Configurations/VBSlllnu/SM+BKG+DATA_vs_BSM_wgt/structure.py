1cb3f84e541be83d467ad1f8908f5028b1f49352
Elena Vernazza <e.vernazza1@campus.unimib.it>
2021-02-16 10:19:40 +0100

    weight

---
+++ Configurations/VBSlllnu/SM+BKG+DATA_vs_BSM_wgt/structure.py
@@ -0,0 +1,50 @@
+# structure configuration for datacard
+
+#structure = {}
+
+# keys here must match keys in samples.py
+#
+
+## Reducible Bkg
+
+structure['sm']  = {'isSignal' : 0, 'isData' : 0}
+
+structure['sm_lin_quad_cqq11']  = {'isSignal' : 1, 'isData' : 0}
+structure['quad_cqq11']  = {'isSignal' : 1, 'isData' : 0}
+
+structure['sm_lin_quad_cqq1']  = {'isSignal' : 1, 'isData' : 0}
+structure['quad_cqq1']  = {'isSignal' : 1, 'isData' : 0}
+
+structure['sm_lin_quad_cqq31']  = {'isSignal' : 1, 'isData' : 0}
+structure['quad_cqq31']  = {'isSignal' : 1, 'isData' : 0}
+
+structure['sm_lin_quad_cqq3']  = {'isSignal' : 1, 'isData' : 0}
+structure['quad_cqq3']  = {'isSignal' : 1, 'isData' : 0}
+
+structure['sm_lin_quad_cW']  = {'isSignal' : 1, 'isData' : 0}
+structure['quad_cW']  = {'isSignal' : 1, 'isData' : 0}
+
+structure['sm_lin_quad_cHl3']  = {'isSignal' : 1, 'isData' : 0}
+structure['quad_cHl3']  = {'isSignal' : 1, 'isData' : 0}
+
+structure['sm_lin_quad_cHq3']  = {'isSignal' : 1, 'isData' : 0}
+structure['quad_cHq3']  = {'isSignal' : 1, 'isData' : 0}
+
+structure['sm_lin_quad_cll1']  = {'isSignal' : 1, 'isData' : 0}
+structure['quad_cll1']  = {'isSignal' : 1, 'isData' : 0}
+
+structure['SSWW']  = {'isSignal' : 0, 'isData' : 0}
+structure['WpWp_QCD']  = {'isSignal' : 0, 'isData' : 0}
+structure['WZ_QCD']  = {'isSignal' : 0, 'isData' : 0}
+structure['ZZ4L']  = {'isSignal' : 0, 'isData' : 0}
+structure['ggZZ']  = {'isSignal' : 0, 'isData' : 0}
+structure['TTV']  = {'isSignal' : 0, 'isData' : 0}
+structure['tZq']  = {'isSignal' : 0, 'isData' : 0}
+structure['Vg']  = {'isSignal' : 0, 'isData' : 0}
+structure['VgS1_L']  = {'isSignal' : 0, 'isData' : 0}
+structure['VgS1_H']  = {'isSignal' : 0, 'isData' : 0}
+structure['DPS']  = {'isSignal' : 0, 'isData' : 0}
+structure['VVV']  = {'isSignal' : 0, 'isData' : 0}
+
+structure['Fake']  = {'isSignal' : 0, 'isData' : 0}
+structure['DATA']  = {'isSignal' : 0, 'isData' : 1}
