d61f6cbcd0c15ee670ca4bed6bce495f3d02dcdf
Elena Vernazza <e.vernazza1@campus.unimib.it>
2021-03-22 13:08:18 +0100

    configuration for 1D analysis

---
+++ Configurations/VBSlllnu/conf_BSM1D/cuts.py
@@ -0,0 +1,20 @@
+# cuts
+# cuts = {}
+#Different supercut used
+
+#supercut for test on succesive selections
+
+## Signal regions
+
+supercut='nLepton>2 && \
+          (abs(Alt$(Lepton_pdgId[0],9999))==11 && abs(Alt$(Lepton_pdgId[1],9999))==11 && abs(Alt$(Lepton_pdgId[2],9999))==13)  || \
+          (abs(Alt$(Lepton_pdgId[0],9999))==13 && abs(Alt$(Lepton_pdgId[1],9999))==11 && abs(Alt$(Lepton_pdgId[2],9999))==11) || \
+          (abs(Alt$(Lepton_pdgId[0],9999))==11 && abs(Alt$(Lepton_pdgId[1],9999))==13 && abs(Alt$(Lepton_pdgId[2],9999))==11)'
+
+## Signal regions
+
+cuts['wz_jetpt30_zlep'] = 'wzinc && bVeto && MET_pt>30 && tauVeto_wz && jetpt30 && abs(detajj)>2.5 && mjj>500 && zlep_wz'
+cuts['wz_jetpt50_zlep'] = 'wzinc && bVeto && MET_pt>30 && tauVeto_wz && jetpt50 && abs(detajj)>2.5 && mjj>500 && zlep_wz'
+
+cuts['wz_zlep'] = 'wzinc && bVeto && MET_pt>20 && tauVeto_wz && jetpt_opt && abs(detajj)>1.5 && mjj>400 && zlep_wz'
+
