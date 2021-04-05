67f598e2ad59e70a7013f07e1bfe6ad66fbecf35
Elena Vernazza <e.vernazza1@campus.unimib.it>
2021-04-05 12:44:39 +0200

    WZin configuration

---
+++ Configurations/VBSlllnu/WZin/conf_WZin_QCD/structure.py
@@ -0,0 +1,9 @@
+# structure configuration for datacard
+
+#structure = {}
+
+# keys here must match keys in samples.py
+
+structure['QCD_SM']  = {'isSignal' : 0, 'isData' : 0}
+
+structure['WZ_QCD']  = {'isSignal' : 0, 'isData' : 0}
---
+++ Configurations/VBSlllnu/WZin/conf_WZin_SM/structure.py
@@ -0,0 +1,9 @@
+# structure configuration for datacard
+
+#structure = {}
+
+# keys here must match keys in samples.py
+
+structure['SM']  = {'isSignal' : 0, 'isData' : 0}
+
+structure['WZ_EWK']  = {'isSignal' : 0, 'isData' : 0}
