97e73648cdf079b9fbf0f4a4132c17d043f42ce8
Elena Vernazza <e.vernazza1@campus.unimib.it>
2021-04-05 12:46:25 +0200

    WZin configuration

---
+++ Configurations/VBSlllnu/WZin/conf_WZin_BSM/cuts.py
@@ -0,0 +1,13 @@
+# cuts
+# cuts = {}
+#Different supercut used
+
+#supercut for test on succesive selections
+
+## Signal regions
+
+supercut='nLepton>2'
+
+cuts['wz_jetpt30_zlep'] = 'wzinc && bVeto && MET_pt>30 && tauVeto_wz && jetpt30 && abs(detajj)>2.5 && mjj>500 && zlep_wz'
+cuts['wz_jetpt50_zlep'] = 'wzinc && bVeto && MET_pt>30 && tauVeto_wz && jetpt50 && abs(detajj)>2.5 && mjj>500 && zlep_wz'
+cuts['wz_zlep'] = 'wzinc && bVeto && MET_pt>20 && tauVeto_wz && jetpt_opt && abs(detajj)>1.5 && mjj>400 && zlep_wz'
