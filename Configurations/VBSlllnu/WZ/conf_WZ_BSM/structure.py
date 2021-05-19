3d855248f16eca03854acf6fd04cfe5f4b5b9dc8
Elena Vernazza <e.vernazza1@campus.unimib.it>
2021-05-19 15:06:25 +0200

    WZ configuration splitted SR

---
+++ Configurations/VBSlllnu/WZ/conf_WZ_BSM/structure.py
@@ -0,0 +1,76 @@
+# structure configuration for datacard
+
+#structure = {}
+
+# keys here must match keys in samples.py
+
+structure['sm']  = {'isSignal' : 1, 'isData' : 0}
+
+# structure['sm_lin_quad_cqq11']  = {'isSignal' : 1, 'isData' : 0}
+# structure['quad_cqq11']  = {'isSignal' : 1, 'isData' : 0}
+
+# structure['sm_lin_quad_cqq1']  = {'isSignal' : 1, 'isData' : 0}
+# structure['quad_cqq1']  = {'isSignal' : 1, 'isData' : 0}
+
+# structure['sm_lin_quad_cqq31']  = {'isSignal' : 1, 'isData' : 0}
+# structure['quad_cqq31']  = {'isSignal' : 1, 'isData' : 0}
+
+# structure['sm_lin_quad_cqq3']  = {'isSignal' : 1, 'isData' : 0}
+# structure['quad_cqq3']  = {'isSignal' : 1, 'isData' : 0}
+
+# structure['sm_lin_quad_cW']  = {'isSignal' : 1, 'isData' : 0}
+# structure['quad_cW']  = {'isSignal' : 1, 'isData' : 0}
+
+# structure['sm_lin_quad_cHl3']  = {'isSignal' : 1, 'isData' : 0}
+# structure['quad_cHl3']  = {'isSignal' : 1, 'isData' : 0}
+
+# structure['sm_lin_quad_cHq3']  = {'isSignal' : 1, 'isData' : 0}
+# structure['quad_cHq3']  = {'isSignal' : 1, 'isData' : 0}
+
+# structure['sm_lin_quad_cll1']  = {'isSignal' : 1, 'isData' : 0}
+# structure['quad_cll1']  = {'isSignal' : 1, 'isData' : 0}
+
+# structure['sm_lin_quad_mixed_cqq1_cqq11'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cqq1_cqq3'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cqq1_cqq31'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cqq11_cqq3'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cqq11_cqq31'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cqq3_cqq31'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cW_cqq1'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cW_cqq11'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cW_cqq3'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cW_cqq31'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cW_cll1'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cll1_cqq1'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cll1_cqq11'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cll1_cqq3'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cll1_cqq31'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cHq3_cqq1'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cHq3_cqq11'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cHq3_cqq3'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cHq3_cqq31'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cHq3_cll1'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cHq3_cW'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cHl3_cqq1'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cHl3_cqq11'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cHl3_cqq3'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cHl3_cqq31'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cHl3_cll1'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cHl3_cW'] = {'isSignal' : 1, 'isData' : 0}
+# structure['sm_lin_quad_mixed_cHl3_cHq3'] = {'isSignal' : 1, 'isData' : 0}
+
+structure['SSWW']  = {'isSignal' : 0, 'isData' : 0}
+structure['WpWp_QCD']  = {'isSignal' : 0, 'isData' : 0}
+structure['WZ_QCD']  = {'isSignal' : 0, 'isData' : 0}
+structure['ZZ4L']  = {'isSignal' : 0, 'isData' : 0}
+structure['ggZZ']  = {'isSignal' : 0, 'isData' : 0}
+structure['TTV']  = {'isSignal' : 0, 'isData' : 0}
+structure['tZq']  = {'isSignal' : 0, 'isData' : 0}
+structure['Vg']  = {'isSignal' : 0, 'isData' : 0}
+structure['VgS1_L']  = {'isSignal' : 0, 'isData' : 0}
+structure['VgS1_H']  = {'isSignal' : 0, 'isData' : 0}
+structure['DPS']  = {'isSignal' : 0, 'isData' : 0}
+structure['VVV']  = {'isSignal' : 0, 'isData' : 0}
+
+structure['Fake']  = {'isSignal' : 0, 'isData' : 0}
+structure['DATA']  = {'isSignal' : 0, 'isData' : 1}
