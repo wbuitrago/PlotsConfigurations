ef5c488a7f22957478e66fa603d607c5467e19f7
Elena Vernazza <e.vernazza1@campus.unimib.it>
2021-05-31 21:20:26 +0200

    plots EFT

--- Configurations/VBSlllnu/WZ/conf_WZ_BSM/plot_BSM.py
+++ Configurations/VBSlllnu/WZ/conf_WZ_BSM/plot_BSM.py
@@ -7,9 +7,13 @@
 #
 
 Red=632; Violet=880; Green=416; Orange=800; Yellow=400; Azure=840; Blue=600; 
+k1=0.5; k2=1; k3=2; 
+color_1=Red; color_2=Violet; color_3=Azure; color_sm=ROOT.kGray+1; color_bkg=ROOT.kGray+2; 
 
 #Signal
-plot['sm']     = dict(color=1, isSignal=1, isData=0, scale=1.0)
+plot['sm']                = dict(color=1, isSignal=1, isData=0, scale=1.0)
+plot['sm_lin_quad_cqq11'] = dict(color=1, isSignal=1, isData=0, scale=1.0)
+plot['quad_cqq11']        = dict(color=1, isSignal=1, isData=0, scale=1.0)
 
 #Reducible Background
 plot['WZ_QCD'] = dict(color=1, isSignal=0, isData=0, scale=1.0)
@@ -23,27 +27,82 @@ plot['VgS1_L'] = dict(color=1, isSignal=0, isData=0, scale=1.0)
 plot['VVV']    = dict(color=1, isSignal=0, isData=0, scale=1.0)
 plot['Fake']   = dict(color=1, isSignal=0, isData=0, scale=1.0)
 
-groupPlot['BKG'] = dict(nameHR='BKG', isSignal=0, color=ROOT.kGray+1, samples=['WZ_QCD','VVV','ZZ4L','TTV','tZq','Vg', 'VgS1_H', 'VgS1_L','Fake'])
-groupPlot['SM']  = dict(nameHR="SM", isSignal=1, color=ROOT.kGray, samples=['sm'])
+groupPlot['BKG'] = {
+    'nameHR' : 'BKG',
+    'isSignal' : 0, 
+    'color' : color_bkg, 
+    'samples' : ['WZ_QCD','VVV','ZZ4L','TTV','tZq','Vg', 'VgS1_H', 'VgS1_L','Fake']
+}
 
-k1=0.1; k2=1; k3=10; 
-cqq1_1=Red; cqq1_2=Violet; cqq1_3=Azure; 
+groupPlot['SM'] = {
+    'nameHR' : 'SM', 
+    'isSignal' : 2, 
+    'color' : color_sm, 
+    'samples' : ['sm']
+}
 
-#EFT components
-plot['sm']                = dict(color=1, isSignal=1, isData=0, scale=1-k1)
-plot['sm_lin_quad_cqq11'] = dict(color=1, isSignal=1, isData=0, scale=k1)
-plot['quad_cqq11']        = dict(color=1, isSignal=1, isData=0, scale=k1*(k1-1))
-groupPlot['cqq11_1']      = dict(nameHR='cqq1 = {}'.format(k1), isSignal=2, color=cqq1_1, samples=['sm','sm_lin_quad_cqq11','quad_cqq11'])
+groupPlot['cqq11_1'] = {
+    'nameHR' : 'cqq11 1= {}'.format(k1),
+    'isSignal' : 2,
+    'color' : color_1,
+    'samples' : ['sm','sm_lin_quad_cqq11','quad_cqq11','WZ_QCD','VVV','ZZ4L','TTV','tZq','Vg', 'VgS1_H', 'VgS1_L','Fake'],
+    'scale' : { 
+                'sm' : 1-k1, 
+                'sm_lin_quad_cqq11' : k1, 
+                'quad_cqq11' : k1*(k1-1),
+                'WZ_QCD' : 1,
+                'VVV' : 1,
+                'ZZ4L' : 1,
+                'TTV' : 1,
+                'tZq' : 1,
+                'Vg' : 1,
+                'VgS1_H' : 1, 
+                'VgS1_L' : 1,
+                'Fake' : 1,
+              },
+}
 
-plot['sm']                = dict(color=1, isSignal=1, isData=0, scale=1-k2)
-plot['sm_lin_quad_cqq11'] = dict(color=1, isSignal=1, isData=0, scale=k2)
-plot['quad_cqq11']        = dict(color=1, isSignal=1, isData=0, scale=k2*(k2-1))
-groupPlot['cqq11_2']      = dict(nameHR='cqq1 = {}'.format(k2), isSignal=2, color=cqq1_2, samples=['sm','sm_lin_quad_cqq11','quad_cqq11'])
+groupPlot['cqq11_2'] = {
+    'nameHR' : 'cqq11 = {}'.format(k2),
+    'isSignal' : 2,
+    'color' : color_2,
+    'samples' : ['sm','sm_lin_quad_cqq11','quad_cqq11','WZ_QCD','VVV','ZZ4L','TTV','tZq','Vg', 'VgS1_H', 'VgS1_L','Fake'],
+    'scale' : { 
+                'sm' : 1-k2, 
+                'sm_lin_quad_cqq11' : k2, 
+                'quad_cqq11' : k2*(k2-1),
+                'WZ_QCD' : 1,
+                'VVV' : 1,
+                'ZZ4L' : 1,
+                'TTV' : 1,
+                'tZq' : 1,
+                'Vg' : 1,
+                'VgS1_H' : 1, 
+                'VgS1_L' : 1,
+                'Fake' : 1,
+              },
+}
 
-plot['sm']                = dict(color=1, isSignal=1, isData=0, scale=1-k3)
-plot['sm_lin_quad_cqq11'] = dict(color=1, isSignal=1, isData=0, scale=k3)
-plot['quad_cqq11']        = dict(color=1, isSignal=1, isData=0, scale=k3*(k3-1))
-groupPlot['cqq11_3']      = dict(nameHR='cqq1 = {}'.format(k3), isSignal=2, color=cqq1_3, samples=['sm','sm_lin_quad_cqq11','quad_cqq11'])
+groupPlot['cqq11_3'] = {
+    'nameHR' : 'cqq11 = {}'.format(k3),
+    'isSignal' : 2,
+    'color' : color_3,
+    'samples' : ['sm','sm_lin_quad_cqq11','quad_cqq11','WZ_QCD','VVV','ZZ4L','TTV','tZq','Vg', 'VgS1_H', 'VgS1_L','Fake'],
+    'scale' : {
+                'sm' : 1-k3, 
+                'sm_lin_quad_cqq11' : k3, 
+                'quad_cqq11' : k3*(k3-1),
+                'WZ_QCD' : 1,
+                'VVV' : 1,
+                'ZZ4L' : 1,
+                'TTV' : 1,
+                'tZq' : 1,
+                'Vg' : 1,
+                'VgS1_H' : 1, 
+                'VgS1_L' : 1,
+                'Fake' : 1,
+              },
+}
 
 # additional options
 legend['lumi'] = 'L = 59.74/fb'
