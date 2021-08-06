3a68743d74f1599d078f645d59ad6480e2c5f566
Elena Vernazza <e.vernazza1@campus.unimib.it>
2021-08-06 10:54:28 +0200

    remove old config WZin

--- Configurations/VBSlllnu/WZin/conf_WZin_QCD/structure.py
+++
@@ -1,9 +0,0 @@
-# structure configuration for datacard
-
-#structure = {}
-
-# keys here must match keys in samples.py
-
-structure['QCD_SM']  = {'isSignal' : 0, 'isData' : 0}
-
-structure['WZ_QCD']  = {'isSignal' : 0, 'isData' : 0}
--- Configurations/VBSlllnu/WZin/conf_WZin_SM/structure.py
+++
@@ -1,9 +0,0 @@
-# structure configuration for datacard
-
-#structure = {}
-
-# keys here must match keys in samples.py
-
-structure['SM']  = {'isSignal' : 0, 'isData' : 0}
-
-structure['WZ_EWK']  = {'isSignal' : 0, 'isData' : 0}
