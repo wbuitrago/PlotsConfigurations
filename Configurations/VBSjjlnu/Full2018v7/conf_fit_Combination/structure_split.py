e0c47694f7ef1ea1646e175942143f23b5be4df3
Giacomo Boldrini <gboldrin@cern.ch>
2021-10-31 18:27:08 +0100

    Charge split of Vbs processes

---
+++ Configurations/VBSjjlnu/Full2018v7/conf_fit_Combination/structure_split.py
@@ -0,0 +1,146 @@
+# structure configuration for datacard
+from itertools import product, chain
+#structure = {}
+
+wjets_bins = []
+for ir in range(1,22):
+    wjets_bins.append("Wjets_res_"+str(ir))
+for ir in range(1,8):
+    wjets_bins.append("Wjets_boost_"+str(ir))
+
+
+phase_spaces_boost = [c for c in cuts if "boost" in c]
+phase_spaces_res = [c for c in cuts if "res" in c]
+
+
+for wbin in wjets_bins:
+    if 'boost' in wbin:
+        structure[wbin] = {
+                    'isSignal' : 0,
+                    'isData'   : 0 ,
+                    'removeFromCuts': phase_spaces_res 
+        }
+    else:
+        structure[wbin] = {
+                    'isSignal' : 0,
+                    'isData'   : 0 ,
+                    'removeFromCuts': phase_spaces_boost 
+        }
+
+
+# structure['Wjets_HT']  = {  
+#                   'isSignal' : 0,
+#                   'isData'   : 0
+#               }
+
+structure['DY']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0
+              }
+
+structure['top']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0
+              }
+
+# structure['VV']  = {  
+#                   'isSignal' : 0,
+#                   'isData'   : 0
+#               }
+for VV_s in VV_samples:
+    structure[VV_s]  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0
+              }
+
+structure['Fake']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0
+              }
+
+
+structure['VVV']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0 
+              }
+
+# structure['VBF-V']  = {  
+#                   'isSignal' : 0,
+#                   'isData'   : 0 
+#               }
+
+structure['VBF-V_dipole']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0 
+              }
+
+structure['ggWW']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0 
+              }
+
+structure['Vg']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0 
+              }
+
+structure['VgS']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0 
+              }
+
+structure['VBS_WpZll']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0 
+              }
+
+structure['VBS_WmZll']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0 
+              }
+
+structure['VBS_ZZ']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0 
+              }
+
+structure['VBS_os_WpToLNu_WmTo2J']  = {  
+                  'isSignal' : 1,
+                  'isData'   : 0 
+              }
+
+structure['VBS_os_WpTo2J_WmToLNu']  = {  
+                  'isSignal' : 1,
+                  'isData'   : 0 
+              }
+
+structure['VBS_WpWp']  = {  
+                  'isSignal' : 1,
+                  'isData'   : 0 
+              }
+
+structure['VBS_WmWm']  = {  
+                  'isSignal' : 1,
+                  'isData'   : 0 
+              }
+
+structure['VBS_WpZjj']  = {  
+                  'isSignal' : 1,
+                  'isData'   : 0 
+              }
+
+structure['VBS_WmZjj']  = {  
+                  'isSignal' : 1,
+                  'isData'   : 0 
+              }
+
+# data
+
+
+structure['DATA']  = { 
+                  'isSignal' : 0,
+                  'isData'   : 1 
+              }
+
+
+samples = {k:v for k,v in samples.items() if "VBS" in k}
