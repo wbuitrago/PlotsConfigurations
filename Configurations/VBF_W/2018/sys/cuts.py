cd986e59364075b06c70baea54774895ca7ecd64
Andrea Bulla <abulla@lxplus717.cern.ch>
2022-06-08 12:42:35 +0200

    update on cuts

--- Configurations/VBF_W/2018/sys/cuts.py
+++ Configurations/VBF_W/2018/sys/cuts.py
@@ -30,28 +30,31 @@ supercut = '((Alt$(abs(CleanJet_eta[0]), 3) <2.5) || (Alt$(abs(CleanJet_eta[0]),
 
 cuts['Z'] = '1'
 
+cuts['do_or_do_not'] = 'Lepton_isTightElectron_mvaFall17V1Iso_WP90[0] > 0.5\
+                        || Lepton_isTightMuon_cut_Tight_HWWW[0] > 0.5'
+
 #cuts['ANWJets'] = 'PuppiMET_pt>50. \
 #                    && CleanJet_pt[1]>60. \
 #                    && bVeto \
 #                    && mtw1 < 50. \
 #                   '
 
-cuts['ele_HWJ'] = '(abs(Lepton_pdgId[0])==11) \
-                    && CleanJet_pt[1]>70. \
-                    && CleanJet_pt[0]<100. \
-                    && bVeto \
-                    && mtw1 > 60. \
-                    && mjj <= 750. \
-                    '
+# cuts['ele_HWJ'] = '(abs(Lepton_pdgId[0])==11) \
+#                     && CleanJet_pt[1]>70. \
+#                     && CleanJet_pt[0]<100. \
+#                     && bVeto \
+#                     && mtw1 > 60. \
+#                     && mjj <= 750. \
+#                     '
 
-cuts['ele_PUWJ'] = '(abs(Lepton_pdgId[0])==11) \
-                    && detajj >= 5. \
-                    && CleanJet_pt[1]<=50.\
-                    && bVeto \
-                    && PuppiMET_pt>70. \
-                    && mtw1>50. \
-                    && CleanJet_pt[0]<70. \
-                    '
+# cuts['ele_PUWJ'] = '(abs(Lepton_pdgId[0])==11) \
+#                     && detajj >= 5. \
+#                     && CleanJet_pt[1]<=50.\
+#                     && bVeto \
+#                     && PuppiMET_pt>70. \
+#                     && mtw1>50. \
+#                     && CleanJet_pt[0]<70. \
+#                     '
 
 cuts['mu_HWJ'] = '(abs(Lepton_pdgId[0])==13) \
                     && CleanJet_pt[1]>70. \
@@ -74,29 +77,52 @@ cuts['mu_PUWJ'] = '(abs(Lepton_pdgId[0])==13) \
 #                && ((Alt$(abs(CleanJet_eta[1]), 3) <2.5) || (Alt$(abs(CleanJet_eta[1]), 0) >3.))\
 #                '
 
-cuts['massiro'] = 'CleanJet_pt[1] < 70. \
-                && bVeto \
-                && PuppiMET_pt > 70. \
-                '
+# cuts['massiro'] = 'CleanJet_pt[1] < 70. \
+#                 && bVeto \
+#                 && PuppiMET_pt > 70. \
+#                 '
                     
-# Top control region
-cuts['topcr']  = '((zeroJet && !bVeto) || bReqTight) \
-                    '
+# # Top control region
+# cuts['topcr']  = '((zeroJet && !bVeto) || bReqTight) \
+#                     '
 
-cuts['skimR'] = 'Lepton_pt[0] >= 30. \
-              && Alt$(CleanJet_pt[1], 0) > 30. \
-              && CleanJet_pt[0] > 100. \
-              && mjj >= 500. \
-              && detajj > 2. \
-              && Lepton_eta[0] > -2 && Lepton_eta[0] < 2'
+# cuts['skimR'] = 'Lepton_pt[0] >= 30. \
+#               && Alt$(CleanJet_pt[1], 0) > 30. \
+#               && CleanJet_pt[0] > 100. \
+#               && mjj >= 500. \
+#               && detajj > 2. \
+#               && Lepton_eta[0] > -2 && Lepton_eta[0] < 2'
 
 ## Fake CR
-#cuts['FakeCR']  = '(mtw1 <= 40.) \
-#                       && PuppiMET_pt<=40. \
-#                       && bVeto\
-#                       && detajj < 4. \
-#                       && CleanJet_pt[1]>60. \
-#                      '
+cuts['ele_FakeCR_isT']  = '(abs(Lepton_pdgId[0])==11) \
+                      && detajj < 5. \
+                      && bVeto\
+                      && mtw1<50. \
+                      && Lepton_isTightElectron_mvaFall17V1Iso_WP90[0] < 0.5 \
+                      && CleanJet_pt[1]<70.  \
+                     '
+    
+cuts['ele_FakeCR']  = '(abs(Lepton_pdgId[0])==11) \
+                      && detajj < 5. \
+                      && bVeto\
+                      && mtw1<50. \
+                      && CleanJet_pt[1]<70.  \
+                     '
+
+cuts['mu_FakeCR_isT']  = '(abs(Lepton_pdgId[0])==13) \
+                      && detajj < 5. \
+                      && bVeto\
+                      && mtw1<50. \
+                      && Lepton_isTightMuon_cut_Tight_HWWW[0] < 0.5 \
+                      && CleanJet_pt[1]<70.  \
+                     '
+    
+cuts['mu_FakeCR']  = '(abs(Lepton_pdgId[0])==13) \
+                      && detajj < 5. \
+                      && bVeto\
+                      && mtw1<50. \
+                      && CleanJet_pt[1]<70.  \
+                     '
 
 ##WJets PU CR
 #cuts['WJetsPUCR']  = 'detajj >= 5. \
