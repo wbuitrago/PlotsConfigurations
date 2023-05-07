bd02781abd21b7715d45cf097f1589e2894c2ecd
Wilson David Buitrago Ceballos <wbuitrag@lxplus790.cern.ch>
2023-05-07 22:24:51 +0200

    first commit

---
+++ Configurations/VBS/2018/ZZCR/ggZZ_Split/configuration_ZZ_ggZZ4t.py
@@ -0,0 +1,46 @@
+# example of configuration file
+treeName= 'Events'
+
+#date='04052023'
+date='_07052023_ggZZ4t'
+
+tag = 'ggZZ4t'+date
+
+
+# used by mkShape to define output directory for root files
+outputDir = 'rootFile'+date
+
+# file with TTree aliases
+aliasesFile = 'aliases.py'
+
+# file with list of variables
+variablesFile = 'variables.py'
+
+# file with list of cuts
+cutsFile = 'cuts.py'
+#cutsFile = 'cuts_forPlots.py'
+
+# file with list of samples
+samplesFile = 'samples_ZZ_ggZZ4t.py'
+
+# file with list of samples
+plotFile = 'plot_ZZ_ggZZ4t.py'
+
+# luminosity to normalize to (in 1/fb)
+lumi = 59.74
+
+# used by mkPlot to define output directory for plots
+# different from "outputDir" to do things more tidy
+outputDirPlots = 'plotVBS'+date
+
+
+# used by mkDatacards to define output directory for datacards
+outputDirDatacard = 'datacards'+date
+
+
+# structure file for datacard
+structureFile = 'structure.py'
+
+
+# nuisances file for mkDatacards and for mkShape
+nuisancesFile = 'nuisances.py'
---
+++ Configurations/VBS/2018/ZZCR/ggZZ_Split/samples_ZZ_ggZZ4t.py
@@ -0,0 +1,107 @@
+import os
+import inspect
+
+configurations = os.path.realpath(inspect.getfile(inspect.currentframe())) # this file
+configurations = os.path.dirname(configurations) # WWSS
+configurations = os.path.dirname(configurations) # 2018
+configurations = os.path.dirname(configurations) # VBS
+configurations = os.path.dirname(configurations) # Configurations
+
+from LatinoAnalysis.Tools.commonTools import getSampleFiles, getBaseWnAOD, addSampleWeight
+
+def nanoGetSampleFiles(inputDir, sample):
+    try:
+        if _samples_noload:
+            return []
+    except NameError:
+        pass
+
+    return getSampleFiles(inputDir, sample, True, 'nanoLatino_')
+
+# samples
+
+try:
+    len(samples)
+except NameError:
+    import collections
+    samples = collections.OrderedDict()
+
+mcProduction = 'Autumn18_102X_nAODv7_Full2018v7'
+mcSteps = 'MCl1loose2018v7__MCCorr2018v7__l2loose__l2tightOR2018v7'
+treeBaseDir = '/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano'
+MCDir = os.path.join(treeBaseDir,  mcProduction , mcSteps)
+
+
+
+# MCDir = '/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/Autumn18_102X_nAODv7_Full2018v7/MCl1loose2018v7__MCCorr2018v7__l2loose__l2tightOR2018v7'
+
+
+################################################
+################### WEIGHTS ####################
+################################################
+
+# ---------------------------------------- Basic MC weights
+mcCommonWeightNoMatch = 'XSWeight*SFweight_mod*samesign_requirement'
+mcCommonWeight = 'XSWeight*SFweight_mod*PromptGenLepMatch2l*samesign_requirement'
+
+# ---------------------------------------- Number of leptons
+Nlep='2'
+#Nlep='3'
+#Nlep='4'
+
+# ---------------------------------------- Lepton WP
+eleWP = 'mvaFall17V1Iso_WP90_SS_tthmva_70'
+# eleWP = 'mvaFall17V1Iso_WP90_SS'
+muWP  = 'cut_Tight_HWWW_tthmva_80'
+#muWP='cut_Tight_HWWW'
+
+LepWPCut        = 'LepCut'+Nlep+'l__ele_'+eleWP+'__mu_'+muWP
+LepWPweight     = 'LepSF'+Nlep+'l__ele_'+eleWP+'__mu_'+muWP
+
+# ---------------------------------------- MET filters
+METFilter_MC   = 'METFilter_MC'
+METFilter_DATA = 'METFilter_DATA'
+
+# ---------------------------------------- fake weights
+#fakeW = 'fakeW2l_ele_'+eleWP+'_mu_'+muWP
+#fakeW = 'fakeW_ele_mvaFall17V1Iso_WP90_SS_tthmva_70_mu_cut_Tight_HWWW_2l2j'
+fakeW = 'fakeW_ele_'+eleWP+'_mu_'+muWP+'_2l2j'
+# ---------------------------------------- Basic MC weights
+XSWeight      = 'XSWeight'
+SFweight      = 'SFweight'+Nlep+'l*'+LepWPweight+'*'+LepWPCut
+PromptGenLepMatch   = 'PromptGenLepMatch'+Nlep+'l'
+
+################################################
+############ DATA DECLARATION ##################
+################################################
+DataRun = [
+            ['A','Run2018A-02Apr2020-v1'] ,
+            ['B','Run2018B-02Apr2020-v1'] ,
+            ['C','Run2018C-02Apr2020-v1'] ,
+            ['D','Run2018D-02Apr2020-v1'] ,
+          ]
+
+DataSets = ['MuonEG','DoubleMuon','SingleMuon','EGamma']
+
+DataTrig = {
+    'MuonEG'         : 'Trigger_ElMu' ,
+    'DoubleMuon'     : '!Trigger_ElMu && Trigger_dblMu' ,
+    'SingleMuon'     : '!Trigger_ElMu && !Trigger_dblMu && Trigger_sngMu' ,
+    'EGamma'         : '!Trigger_ElMu && !Trigger_dblMu && !Trigger_sngMu && (Trigger_sngEl || Trigger_dblEl)' ,
+}
+
+
+################################################
+################ MC SAMPLES ####################
+################################################
+
+# ---------------------------------------- ggZZ 
+
+files = nanoGetSampleFiles(MCDir, 'ggZZ4t')
+samples['ggZZ4t'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 4
+}
+
+
